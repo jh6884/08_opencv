5a3ceaf39e006c65a3da38a1b1fc3262f0da3909
shinjaehyeon <pumpkinalpha996@gmail.com>
2025-08-06 17:28:35 +0900

    .json 데이터를 활용해 이미지 분류

--- src/file_checker.py
+++ src/file_checker.py
@@ -13,9 +13,14 @@ def file_match(directory, key_to_check, value_to_check, destination):
                 for item in data["annotation"]:
                     # 딕셔너리 내부에 원하는 키가 있는지 확인합니다.
                     if key_to_check in item and item[key_to_check] == value_to_check:
-                        shutil.move(dir+filename, destination)
-                        print('파일 이동 성공')
+                        paths = '../img/daylight/'+filename[:9]+'.jpg'
+                        try:
+                            print('../img/daylight/'+filename[:9]+'.jpg 이동 시도')
+                            shutil.move(paths, destination)
+                            print(filename[:9]+'.jpg 파일 이동 성공')
+                        except:
+                            continue
     return
 
-file_match(dir, "shape", "triangle", "../img/road_sign/triangle")
-file_match(dir, "shape", "circle", "../img/road_sign/circle")
+file_match(dir, "shape", "triangle", "../img/road_sign/triangle/")
+file_match(dir, "shape", "circle", "../img/road_sign/circle/")
